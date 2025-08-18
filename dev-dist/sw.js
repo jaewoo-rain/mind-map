c0ea54210a3553d4b0d97a748050d235abee28cc
jaewoo <202017499@jbnu.ac.kr>
2025-08-18 18:14:16 +0900

    다시 원복;

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.06fe33s9e48"
+    "revision": "0.t0musofslcg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
