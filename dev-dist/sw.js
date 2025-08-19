fdfe44f064eacaf05b9c323d2e17b86cd6f40f69
jaewoo <202017499@jbnu.ac.kr>
2025-08-20 01:46:47 +0900

    course5 스팟 더미데이터 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0he2mb77v8"
+    "revision": "0.ult9podrvu8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
