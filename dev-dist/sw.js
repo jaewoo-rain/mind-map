108dca56db51d327d6d6faae7f42b3c60b65f41f
jaewoo <202017499@jbnu.ac.kr>
2025-08-20 03:09:48 +0900

    recommend UI 동적으로 만드는중

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cg3r871ikp"
+    "revision": "0.436s23iumpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
