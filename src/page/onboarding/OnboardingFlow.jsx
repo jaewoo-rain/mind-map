980812dd5a11973486fb593eba0bec0e0b643535
jaewoo <202017499@jbnu.ac.kr>
2025-08-20 02:15:02 +0900

    nickNamePage 오류

--- src/page/onboarding/OnboardingFlow.jsx
+++ src/page/onboarding/OnboardingFlow.jsx
@@ -1,7 +1,7 @@
 // src/OnboardingFlow.jsx
 import React, { useMemo, useState } from "react";
 
-import NicknamePage from "./NickNamePage.jsx";
+import NicknamePage from "./InputInfoPage.jsx";
 import GenderAgePage from "./GenderAgePage";
 import TravelPreferencePage from "./TravelPreferencePage";
 import RegionSelectPage from "./RegionSelectPage";
