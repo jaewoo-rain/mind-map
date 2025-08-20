012c3ca8236760073dbe8406d84b091748dd5623
jaewoo <202017499@jbnu.ac.kr>
2025-08-21 02:42:49 +0900

    storyFeed

--- src/page/Camera/StoryFeedPage.jsx
+++ src/page/Camera/StoryFeedPage.jsx
@@ -9,54 +9,58 @@ const mockStories = [
     id: 1,
     type: "spot_discovery",
     imageSrc:
-      "https://images.unsplash.com/photo-1572052800318-3f4007a8b4a7?w=500",
+      "https://images.pexels.com/photos/33425330/pexels-photo-33425330.jpeg",
     author: "JejuExplorer",
     avatar: "https://placehold.co/30x30/E8A978/FFFFFF?text=J",
     timeAgo: "1시간 전",
-    photo: "https://images.unsplash.com/photo-1572052800318-3f4007a8b4a7?w=500",
+    photo:
+      "https://images.pexels.com/photos/33425330/pexels-photo-33425330.jpeg",
     caption: "여기 사진 정말 잘 나와요!",
   },
   {
     id: 2,
     type: "timer_story",
     imageSrc:
-      "https://images.unsplash.com/photo-1517838277531-41214a1a3a35?w=500",
+      "https://images.pexels.com/photos/9638704/pexels-photo-9638704.jpeg",
     author: "열정러너",
     avatar: "https://placehold.co/30x30/A5A5A5/FFFFFF?text=R",
     timeAgo: "3시간 전",
-    photo: "https://images.unsplash.com/photo-1517838277531-41214a1a3a35?w=500",
+    photo: "https://images.pexels.com/photos/9638704/pexels-photo-9638704.jpeg",
     caption: "오늘의 기록",
   },
   {
     id: 3,
     type: "plain",
     imageSrc:
-      "https://images.unsplash.com/photo-1533105079780-52b9be4ac26c?w=500",
+      "https://cdn.pixabay.com/photo/2014/09/18/17/29/sea-451168_1280.jpg",
     author: "바다사랑",
     avatar: "https://placehold.co/30x30/3498db/FFFFFF?text=S",
     timeAgo: "5시간 전",
-    photo: "https://images.unsplash.com/photo-1533105079780-52b9be4ac26c?w=500",
+    photo: "https://cdn.pixabay.com/photo/2014/09/18/17/29/sea-451168_1280.jpg",
     caption: "해변 러닝은 진리",
   },
   {
     id: 4,
     type: "plain",
-    imageSrc: "https://images.unsplash.com/photo-1542037104-924825a12622?w=500",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2019/05/27/10/06/silver-grass-4232359_1280.jpg",
     author: "산들바람",
     avatar: "https://placehold.co/30x30/2ecc71/FFFFFF?text=B",
     timeAgo: "어제",
-    photo: "https://images.unsplash.com/photo-1542037104-924825a12622?w=500",
+    photo:
+      "https://cdn.pixabay.com/photo/2019/05/27/10/06/silver-grass-4232359_1280.jpg",
     caption: "오름 정상에서",
   },
   {
     id: 5,
     type: "plain",
     imageSrc:
-      "https://cdn.pixabay.com/photo/2014/09/18/17/29/sea-451168_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/02/06/08/53/lighthouse-1182680_1280.jpg",
     author: "돌하르방",
     avatar: "https://placehold.co/30x30/95a5a6/FFFFFF?text=D",
     timeAgo: "어제",
-    photo: "https://cdn.pixabay.com/photo/2014/09/18/17/29/sea-451168_1280.jpg",
+    photo:
+      "https://cdn.pixabay.com/photo/2016/02/06/08/53/lighthouse-1182680_1280.jpg",
     caption: "제주 시내 야경",
   },
 ];
